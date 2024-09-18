a56d91c6e7b5b4d5c2b8798915d6aef4b5d22d99
Kevyn San Juan <ksanjuan.000@gmail.com>
2024-09-19 00:38:20 +0800

    added mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "/images/Maia Bakery Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,13 +12,21 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <header className="flex justify-between max-w-5xl m-auto font-mono px-5 text-[--text-color] fixed top-0 left-0 right-0 backdrop-opacity-20 backdrop-invert bg-[--bg-color]/30 md:rounded-xl md:mt-2">
+      {/* backdrop-opacity-20 backdrop-invert bg-[--bg-color]/30 */}
+      <header className="flex justify-between max-w-5xl m-auto font-mono px-5 bg-[#c78a855e] text-[--text-color] fixed top-0 left-0 right-0  md:rounded-xl md:mt-2">
         <Link to="/" className="">
           <img src={Logo} className="w-28 h-auto" />
         </Link>
 
+        {/* NAVBAR LINKS */}
         <ul className="hidden md:flex gap-5 items-center text-xl">
           <li>
             <Link to="/" className="hover:text-[--main-color]">
@@ -42,43 +50,75 @@ const Header = () => {
           </li>
         </ul>
 
-        <div className="flex gap-5 items-center text-xl">
-          <a href="#">
+        {/* MOBILE MENU */}
+        {isOpen && (
+          <div className="md:hidden bg-[#c78a855e] rounded-lg flex flex-col gap-10 text-center absolute mt-[109px] w-[560px]">
+            <Link
+              to="/"
+              className="block text-black text-3xl hover:text-[--main-color]"
+            >
+              Home
+            </Link>
+            <Link
+              to="/shop"
+              className="block text-black text-3xl hover:text-[--main-color]"
+            >
+              Shop
+            </Link>
+            <Link
+              to="/about"
+              className="block text-black text-3xl hover:text-[--main-color]"
+            >
+              About
+            </Link>
+            <Link
+              to="/contact"
+              className="block text-black text-3xl hover:text-[--main-color]"
+            >
+              Contact
+            </Link>
+          </div>
+        )}
+
+        <div className="flex gap-5 items-center text-4xl">
+          {/* <a href="#">
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
               className="hover:text-[--main-color]"
             />
-          </a>
-          <a href="#">
+          </a> */}
+          {/* <a href="#">
             <FontAwesomeIcon
               icon={faBell}
               className="hover:text-[--main-color]"
             />
-          </a>
-          <a href="#">
+          </a> */}
+          {/* <a href="#">
             <FontAwesomeIcon
               icon={faCartShopping}
               className="hover:text-[--main-color]"
             />
-          </a>
+          </a> */}
           <div className="bx bx-menu md:hidden" id="menu-icon">
             <a href="#">
               <FontAwesomeIcon
                 icon={faBars}
                 className="hover:text-[--main-color]"
+                onClick={toggleMenu}
+                type="button"
               />
             </a>
           </div>
 
           {/* ----------darkmode---------- */}
-          <div className="bx bx-moon" id="darkmode">
+          {/* <div className="bx bx-moon" id="darkmode">
             <a href="#">
               <FontAwesomeIcon
                 icon={faMoon}
                 className="hover:text-[--main-color]"
               />
             </a>
-          </div>
+          </div> */}
         </div>
       </header>
     </div>
